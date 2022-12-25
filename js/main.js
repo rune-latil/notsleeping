3ff5edc2cf8fe71e3f12cfc38388c4a4214a3ed0
ssimplay <ssimplay@naver.com>
2022-12-25 19:07:56 +0900

    type: Fix main content - talkBox button 반복생성 fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,51 @@
 const main = document.querySelector("#main");
 const talk = document.querySelector("#talk");
+const result = document.querySelector("#result");
+const endPoint = 16;
 
-function goNext() {
+function textBox(talkText, tIdx) {
+    var talkBox = document.querySelector(".talkBox");
+    var talkLi = document.createElement('li');
+
+    talkLi.classList.add('talkLi');
+    talkBox.appendChild(talkLi);
+    talkLi.innerHTML = talkText;
+
+    talkLi.addEventListener("click", function () {
+        var children = document.querySelectorAll('.talkLi');
+        for (let i = 0; i < children.length; i++) {
+            children[i].disabled = true;
+            children[i].style.display = 'none';
+        }
+        goNext(++tIdx);
+    }, false);
+
+}
+
+function goResult() {
+    talk.style.Webkitanimation = "fadeOut 1s";
+    talk.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+        result.style.Webkitanimation = "fadeIn 1s";
+        result.style.animation = "fadeIn 1s";
+        setTimeout(() => {
+            talk.style.display = "none";
+            result.style.display = "block";
+        }, 600)
+    });
+}
+
+
+function goNext(tIdx) {
+    if (tIdx + 1 === endPoint) {
+        goResult();
+    }
     var img = document.querySelector(".imgBox");
-    var name = document.querySelector(".name");
-    var talkText = document.querySelector(".talkText");
+    img.innerHTML = talkList[tIdx].i;
 
-    img.innerHTML = talkList[0].i;
-    name.innerHTML = talkList[0].t[0].name;
-    talkText.innerHTML = talkList[0].t[1].talk;
+    for (let i in talkList[tIdx].t) {
+        textBox(talkList[tIdx].t[i].text, tIdx);
+    }
 
 }
 
@@ -22,7 +59,8 @@ function start() {
             main.style.display = "none";
             talk.style.display = "flex";
         }, 600)
-        goNext();
+        let tIdx = 0;
+        goNext(tIdx);
     });
 
 }
